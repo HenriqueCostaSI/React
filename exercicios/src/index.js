f378262455e2e6437b36b1b1cba473d62553aaca
henriqueCostaSI <henrique.mcosta@hotmail.com>
2025-01-23 23:12:40 -0300

    setState

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import  Multi, {  BoaNoite } from "./componentes/Multiplos";
+import Saudacoes from "./componentes/Saudacoes";
+
 
 ReactDOM.render(
+  <div>
+    <Saudacoes tipo="Bom dia" nome="Ana" />
+  </div>,
+  document.getElementById("root")
+);
+
+
+
+//import  Multi, {  BoaNoite } from "./componentes/Multiplos";
+
+
+/* ReactDOM.render(
   <div>
     <Multi.BoaTarde nome="Ana" />
     <BoaNoite nome="Bia" />
   </div>,
   document.getElementById("root")
-);
+); */
