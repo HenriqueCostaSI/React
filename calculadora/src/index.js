2849f3db0c94ad0698fc08e92e220c7866204781
henriqueCostaSI <henrique.mcosta@hotmail.com>
2025-01-28 22:34:01 -0300

    Remove arquivos App.js e App.css, atualiza index.js para exibir "Calculadora" e ajusta o componente Button para suportar múltiplas operações.

--- calculadora/src/index.js
+++ calculadora/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals'
 import Calculator from './main/Calculator'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1>Hello World</h1>
+    <h1>Calculadora</h1>
     <Calculator />
   </div>
 );
