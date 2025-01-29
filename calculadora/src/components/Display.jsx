88783e2cc5e023331d5c040f738cb7f0df09ef3b
henriqueCostaSI <henrique.mcosta@hotmail.com>
2025-01-28 23:16:48 -0300

    correcoes

--- calculadora/src/components/Display.jsx
+++ calculadora/src/components/Display.jsx
@@ -1,5 +1,12 @@
-import React from 'react'
-import './Display.css'
+import React from 'react';
+import './Display.css';
 
-export default props =>
-    <div className="display">{props.value}</div>
+const Display = (props) => {
+    return (
+        <div className="display">
+            {props.value}
+        </div>
+    );
+};
+
+export default Display;
